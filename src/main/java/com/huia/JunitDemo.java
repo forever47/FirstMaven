f615ea2da7742925c20a1b2fa7de6aaa36e3ad5a
wyn <194103205@qq.com>
2018-12-19 16:20:19 +0800

    Initial commit

---
+++ src/main/java/com/huia/JunitDemo.java
@@ -0,0 +1,7 @@
+package com.huia;
+
+public class JunitDemo {
+    public void testPrint(){
+        System.out.println("WE ARE AQUAMAN");
+    }
+}
---
+++ src/test/java/com/huia/JunitDemoTest.java
@@ -0,0 +1,14 @@
+package com.huia;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JunitDemoTest {
+
+    @Test
+    public void testPrint() {
+        JunitDemo junitDemo=new JunitDemo();
+        junitDemo.testPrint();
+    }
+}
